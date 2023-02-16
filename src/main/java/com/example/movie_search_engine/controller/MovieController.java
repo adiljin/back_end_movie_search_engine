9b13f557e90dc9932cf4d43258244b60573146bb
adiljin <adiljins@gmail.com>
2023-02-16 15:25:26 -0500

    Initial commit

---
+++ src/main/java/com/example/movie_search_engine/controller/MovieController.java
@@ -0,0 +1,52 @@
+package com.example.movie_search_engine.controller;
+
+import com.example.movie_search_engine.model.Movie;
+import com.example.movie_search_engine.repository.MovieRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/movies")
+@CrossOrigin("http://localhost:3000/")
+public class MovieController {
+
+    @Autowired
+    private MovieRepo movieRepo;
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping()
+    public List<Movie> searchMovieStatic() {
+        return movieRepo.findAll();
+    }
+
+    @GetMapping("/{title}")
+    public List<Movie> searchMovie(@PathVariable String title) {
+        String url = UriComponentsBuilder.fromUriString("http://www.omdbapi.com/?apikey=339381a4&")
+                .queryParam("t", title)
+                .toUriString();
+
+        List<Movie> list = new ArrayList<>();
+        Movie movie = restTemplate.getForObject(url, Movie.class);
+        list.add(movie);
+        System.out.println(url);
+        System.out.println(movie.getTitle());
+        return list;
+    }
+
+    @PostMapping()
+    Movie newMovie(@RequestBody List<Movie> newMovie){
+        Movie movie = newMovie.get(0);
+        return movieRepo.save(movie);
+    }
+
+}
+
+
+
+
---
+++ src/main/java/com/example/movie_search_engine/model/Movie.java
@@ -0,0 +1,64 @@
+package com.example.movie_search_engine.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="MOVIE")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonProperty("Title")
+    private String title;
+
+    @JsonProperty("Year")
+    private String year;
+
+    @JsonProperty("Rated")
+    private String rated;
+
+    @JsonProperty("Released")
+    private String released;
+
+    @JsonProperty("Runtime")
+    private String runtime;
+
+    @JsonProperty("Genre")
+    private String genre;
+
+    @JsonProperty("Director")
+    private String director;
+
+    @JsonProperty("Writer")
+    private String writer;
+
+    @JsonProperty("Actors")
+    private String actors;
+
+    @JsonProperty("Plot")
+    private String plot;
+
+    @JsonProperty("Language")
+    private String language;
+
+    @JsonProperty("Country")
+    private String country;
+
+    @JsonProperty("Awards")
+    private String awards;
+
+    @JsonProperty("Poster")
+    private String poster;
+
+    @JsonProperty("imdbRating")
+    private String imdbRating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    // getters and setters
+}
